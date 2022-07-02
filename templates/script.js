6b84ea9787ba3d64624db1daf5a9f66134012057
Marcel <69498290+marsel3@users.noreply.github.com>
2022-07-02 14:09:54 +0300

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,11 @@
+
+
+
+function ValidPas() {
+    let login = document.getElementById('input_login');
+    let password = document.getElementById('password_login');
+
+    if (login == 'ivan' && password == 'ivan')
+        alert('Welcome');
+    else alert('Go Fuck yourself!!');
+}
