48322266d580029735b10c3449d32410aad50d7e
marcel3 <akbulatov03@mail.ru>
2022-07-04 13:26:29 +0300

    marsel

--- templates/script.js
+++ templates/script.js
@@ -2,15 +2,34 @@
 
 
 function ValidPas() {
-    let login = document.getElementById('input_login');
-    let password = document.getElementById('password_login');
+    let login = document.getElementById('input_email').value;
+    let password = document.getElementById('input_password').value;
 
-    if (login == 'ivan' && password == 'ivan')
-        alert('Welcome');
-    else alert('Go Fuck yourself!!');
+    if (login == '') alert('Заполните строку с e-mail');
+    else if (password == '') alert('Заполните строку с паролем');
+    else if (login == 'ivan' && password == 'ivan') {
+        alert('Вы успешно авторизовались!');
+        window.location.href = 'main.html';
+        }
+    else alert('Неверное имя пользователя или пароль.');
 }
 
 
+
+function Registration() {
+    let name = document.getElementById('input_name_reg').value;
+    let login = document.getElementById('input_email_reg').value;
+    let password = document.getElementById('input_password_reg').value;
+    let password2 = document.getElementById('input_password_reg_2').value;
+
+    if (name == '') alert('Заполните строку с именем');
+    else if (login == '') alert('Заполните строку с e-mail');
+    else if (password == '') alert('Заполните строку с паролем');
+    else if (password2 == '') alert('Введите пароль снова');
+    else if (password != password2 && password != '') alert('Пароли не совпадают!');
+    else {alert('Регистрация прошла успешно!');
+          window.location.href = 'main.html';}
+}
 
 
 
@@ -34,4 +53,20 @@ setInterval(function()
     else if (r4.checked) {document.getElementById("r4").checked = false;
         document.getElementById("r1").checked = true;}
 
-}, 2000);
+}, 5000);
+
+
+function SendForm() {
+    let name = document.getElementById('name_input_form').value;
+    let email = document.getElementById('email_input_form').value;
+    let text = document.getElementById('text_input_form').value;
+
+    if (name == '') alert('Заполните строку с вашим именем');
+    else if (email == '') alert('Заполните строку с вашим телефоном или е-mail');
+    else if (text == '') alert('Заполните строку с собщением');
+
+    else {
+            alert('Ваш запрос был успешно отправлен администрации магазина!');
+            window.location.href = 'main.html';
+          }
+}
